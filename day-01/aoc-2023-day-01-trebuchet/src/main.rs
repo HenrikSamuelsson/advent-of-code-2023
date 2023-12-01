816ba376507d3c745ef37a8b1249f453cbb5eb74
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-12-01 06:28:14 +0100

    Add code to open file with input data.

--- day-01/aoc-2023-day-01-trebuchet/src/main.rs
+++ day-01/aoc-2023-day-01-trebuchet/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+
+fn main() -> Result<(), std::io::Error> {
+    let file = File::open("aoc-2023-day-01-input.txt")?;
+    print!("{:?}",file);
+    Ok(())
 }
