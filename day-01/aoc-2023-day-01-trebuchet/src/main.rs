8f574f5fb0d9eb69daf4a8fc43a300551af14809
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-12-02 05:37:39 +0100

    New way of reading lines based on Rust By Example.

--- day-01/aoc-2023-day-01-trebuchet/src/main.rs
+++ day-01/aoc-2023-day-01-trebuchet/src/main.rs
@@ -1,7 +1,23 @@
 use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-fn main() -> Result<(), std::io::Error> {
-    let file = File::open("aoc-2023-day-01-input.txt")?;
-    print!("{:?}",file);
-    Ok(())
+fn main() {
+    // File aoc-2023-day-01-input.txt must exist in the current path.
+    if let Ok(lines) = read_lines("./aoc-2023-day-01-input.txt") {
+        // Consumes the iterator, returns an (Optional) String.
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
