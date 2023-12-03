4438617990ec8a19250c96d389b1f8f84814da75
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-12-03 08:40:10 +0100

    Read in example file data to a buffer.

--- aoc-year-2023-day-03-gear-ratios/src/main.rs
+++ aoc-year-2023-day-03-gear-ratios/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // File must exist in the current path.
+    if let Ok(_lines) = read_lines("./example-puzzle-input.txt") {
+        println!("Read puzzle example input.")
+    }
+    else {
+        println!("Error reading lines from file.");
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
