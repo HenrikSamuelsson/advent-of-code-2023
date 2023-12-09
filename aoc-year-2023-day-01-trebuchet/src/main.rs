b0aecb4ca8c264802d669f5b26333851744ccb73
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-12-09 21:11:41 +0100

    Add first version of code that solves part 2.

--- aoc-year-2023-day-01-trebuchet/src/main.rs
+++ aoc-year-2023-day-01-trebuchet/src/main.rs
@@ -5,26 +5,82 @@ use std::path::Path;
 fn main() {
     // File must exist in the current path.
     if let Ok(lines) = read_lines("./example-puzzle-input.txt") {
-        // Consumes the iterator, returns an (Optional) String.
-        let mut sum = 0;
+        let mut sum_part_1 = 0;
         for line in lines {
-            let first_digit;
-            let last_digit;
-            if let Ok(amended_line) = line {
+            let mut first_digit = 0;
+            let mut last_digit = 0;
+            if let Ok(ref amended_line) = line {
                 println!("{}", amended_line);
-                first_digit = find_first_digit(&amended_line);
-                last_digit = find_last_digit(&amended_line);
+                if let Some(digit) = find_first_digit(&amended_line) {
+                    first_digit = digit;
+                }
+                if let Some(digit) = find_last_digit(&amended_line) {
+                    last_digit = digit;
+                }
 
                 println!("first_digit = {}", first_digit);
                 println!("last_digit = {}", last_digit);
 
-                sum += 10 * first_digit + last_digit;
+                sum_part_1 += 10 * first_digit + last_digit;
             }
         }
-        println!("Answer part 1 = {}", sum);
+        println!("Answer part 1 = {}", sum_part_1);
     } else {
         println!("Error reading lines from file.");
     }
+    
+
+    // Part 2
+    let mut sum_part_2 = 0;
+    if let Ok(lines2) = read_lines("./example-puzzle-input2.txt") {
+        for line in lines2 {
+            let mut first_digit = 0;
+            let mut last_digit = 0;
+            if let Ok(ref amended_line2) = line {
+                println!("{}", amended_line2);
+                let mut chars = amended_line2.chars();
+                loop {
+                    if let Some(digit) = parse_number_at_start(&chars.as_str()) {
+                        first_digit = digit;
+                        println!("first_digit parsed {}", first_digit);
+                        break;
+                    }
+                    if let Some(ch) = chars.next() {
+                        if let Some(digit) = ch.to_digit(10) {
+                            first_digit = digit;
+                            println!("first_digit single char {}", first_digit);
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                chars = amended_line2.chars();
+                loop {
+                    if let Some(digit) = parse_number_at_end(&chars.as_str()) {
+                        last_digit = digit;
+                        println!("first_digit parsed {}", first_digit);
+                        break;
+                    }
+                    if let Some(ch) = chars.next_back() {
+                        if let Some(digit) = ch.to_digit(10) {
+                            last_digit = digit;
+                            println!("first_digit single char {}", first_digit);
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                sum_part_2 += 10 * first_digit + last_digit;
+            }
+        }
+    } else {
+        println!("Error reading lines from file.");
+    }
+    println!("Answer part 2 = {}", sum_part_2);
 }
 
 // The output is wrapped in a Result to allow matching on errors.
@@ -38,25 +94,23 @@ where
 }
 
 /// Finds the first digit in a given string.
-/// Note, will return 0 if no digit is found in the string.
-fn find_first_digit(text: &str) -> u32 {
+fn find_first_digit(text: &str) -> Option <u32> {
     for c in text.chars() {
         if c.is_numeric() {
-            return c.to_digit(10).unwrap();
+            return Some(c.to_digit(10).unwrap());
         }
     }
-    0
+    None
 }
 
 /// Finds the last digit in a given string.
-/// Note, will return 0 if no digit is found in the string.
-fn find_last_digit(text: &str) -> u32 {
+fn find_last_digit(text: &str) -> Option<u32> {
     for c in text.chars().rev() {
         if c.is_numeric() {
-            return c.to_digit(10).unwrap();
+            return Some(c.to_digit(10).unwrap());
         }
     }
-    0
+    None
 }
 
 /// Finds letter combinations corresponding to one digit numbers at the start of a given string.
