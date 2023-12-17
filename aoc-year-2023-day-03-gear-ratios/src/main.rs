e11f31916693185fc9842610fa8830e9e4ce21d8
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-12-17 10:40:23 +0100

    Add code.

--- aoc-year-2023-day-03-gear-ratios/src/main.rs
+++ aoc-year-2023-day-03-gear-ratios/src/main.rs
@@ -3,6 +3,22 @@ fn main() {
 }
 
 pub fn solve_part_1(_input_file_name: &str) -> u32 {
+    // Create a path to the desired file
+    let path = Path::new(input_file_name);
+
+    // Open the path in read-only mode.
+    let file = match File::open(&path) {
+        Err(why) => panic!(
+            "Could not open input file: {}",
+            <dyn Error>::to_string(&why)
+        ),
+        Ok(file) => file,
+    };
+
+    let mut result: u32 = 0;
+
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
     0
 }
 
