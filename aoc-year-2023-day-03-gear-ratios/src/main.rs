a8e74432db6151df71d8e55889dc2b914fb590c1
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-12-18 08:59:27 +0100

    Add storage of symbols in a vector.

--- aoc-year-2023-day-03-gear-ratios/src/main.rs
+++ aoc-year-2023-day-03-gear-ratios/src/main.rs
@@ -5,21 +5,33 @@ fn main() {
 }
 
 pub fn solve_part_1(input_file_name: &str) -> u32 {
-    let lines = lines_from_file(input_file_name);   
-    for (y, line) in lines.iter().enumerate() {
+    // Read the complete engine schematic from file.
+    let schematic = schematic_from_file(input_file_name);
+    
+    // Vector for holding all the symbol locations of the engine schematic.
+    let mut schematic_symbol_locations: Vec<(u32, u32)> = vec![];
+    // Loop to search the for symbols in the schematic.
+    for (y, line) in schematic.iter().enumerate() {
         println!("{:?}", line);
         for (x, c) in line.chars().enumerate() {
             if !c.is_numeric() {
                 if c != '.' {
+                    let x = x.try_into().unwrap();
+                    let y = y.try_into().unwrap();
+                    schematic_symbol_locations.push((x, y));
                     println!("{} {} {}", c, x, y);
                 }
             }
         }
     }
+
+    // Vector for holding all the numbers adjacent to a symbol.
+    let mut schematic_symbol_locations: Vec<(u32, u32)> = vec![];
+    // Loop to search for any number adjacent to a symbol.
     0
 }
 
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+fn schematic_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
     buf.lines()
