4b9068c6da2eed9e8e7d63499c64adcf98edcb1d
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-01-01 08:14:18 +0100

    Add unimplemented solve function and a unit test for this.

--- aoc-year-2023-day-14-parabolic-reflector-dish/src/main.rs
+++ aoc-year-2023-day-14-parabolic-reflector-dish/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("example-puzzle-input.txt").unwrap();
+}
+
+fn solve_part_1(input_file_name: &str)->i32 {
+    0
+}
+
+#[test]
+fn test_solve_part_1() {
+    assert_eq!(solve_part_1("example-puzzle-input.txt"), 136)
 }
