71000a85a7a2e0edfae35d87ab47189e766a123c
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-01-06 12:50:50 +0100

    Add function that maps rocks.

--- aoc-year-2023-day-14-parabolic-reflector-dish/src/main.rs
+++ aoc-year-2023-day-14-parabolic-reflector-dish/src/main.rs
@@ -1,10 +1,5 @@
 use std::{io::{BufReader, BufRead}, fs::File, path::Path, collections::HashMap};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Grid {
-    grid: Vec<Vec<char>>,
-}
-
 fn main() {
     let result_part_1 = solve_part_1("example-puzzle-input.txt");
     println!("Result part 1 = {}", result_part_1);
@@ -42,4 +37,24 @@ fn test_read_input_file() {
     assert_eq!(rows[0], "O....#....");    // Assert that first line is the expected.
     assert_eq!(rows[9], "#OO..#....");    // Assert that last line is the expected. 
     assert_eq!(rows.len(), 10);           // Assert that number of lines is the expected.
+}
+
+fn create_rocks_map(rock_rows: Vec<String>) -> HashMap<(i32, i32), char> {
+    let mut rocks_map: HashMap<(i32, i32), char> = HashMap::new();
+    for (y, row) in rock_rows.iter().enumerate() {
+        for (x, c) in row.chars().enumerate() {
+            rocks_map.insert((x.try_into().unwrap(), y.try_into().unwrap()), c);
+        }
+    }
+    rocks_map
+} 
+
+#[test]
+fn test_create_rocks_map() {
+    let rock_rows: Vec<String> = read_input_file("example-puzzle-input.txt");
+    let rocks_map = create_rocks_map(rock_rows);
+    assert_eq!(rocks_map.get(&(0, 0)), Some(&'O'));    // Assert that north-west corner holds 'O'.
+    assert_eq!(rocks_map.get(&(9, 0)), Some(&'.'));    // Assert that north-east corner holds '.'.
+    assert_eq!(rocks_map.get(&(0, 9)), Some(&'#'));    // Assert that south-west corner holds '#'.
+    assert_eq!(rocks_map.get(&(9, 9)), Some(&'.'));    // Assert that south-east corner holds '.'.
 }
